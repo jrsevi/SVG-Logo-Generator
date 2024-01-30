c9153d1cafecaebaf4fd77252e29ae53bf4e9946
jrsevi <jrsevi@gmail.com>
2024-01-29 17:08:38 -0800

    Added index.js, library folder, and logos folder

---
+++ index.js
@@ -0,0 +1,60 @@
+const { Circle, Square, Triangle } = require('./library/shapes.js');
+
+const fs = require('fs');
+
+const inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type:"list",
+        name:"shape",
+        message: "Please select a shape",
+        choices: ["Circle", "Square", "Triangle"] 
+    },
+    {
+        type:"input",
+        name:"color",
+        message: "Please enter a color"
+    },
+    {
+        type: 'input',
+        name: 'text',
+        validate: function (value) {
+            if (value.length === 3) {
+                return.true;
+            }
+            return 'Please enter 3 characters';
+        },
+        message: 'Please enter 3 characters'
+    }
+]).then(function (answers) {
+    let shape;
+    let text = answers.text;
+    let color = answers.color;
+    if (color === "") color = "black";
+    switch (answers.shape) {
+        case "Circle":
+            shape = new Circle(answers.color);
+            break;
+        case "Square":
+            shape = new Square(answers.color);
+            break;
+        case "Triangle":
+            shape = new Triangle(answers.color);
+            break;
+    }
+    fs.writeFile("logos/" + answers.shape + ".svg", renderSvg(shape, text), function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("File Saved Sucessfully!");
+        }
+        });
+    });
+
+    function renderSvg(shape, text) {
+        return '<svg width="200" height="200">
+        ${shape.render()}
+        <text x="50%" y="50%" text-anchor="middle" font-size="2em" alignment-baseline="middle" fill="back">${text}</text>
+        </svg>';
+    }
---
+++ library/shapes.js
@@ -0,0 +1,43 @@
+class Shapes {
+    constructor(color) {
+        this.color = color;
+    }
+
+}
+
+class Circle extends Shapes {
+    constructor(color) {
+        super(color);
+        this.radius = 50;
+        this.cx = 50;
+        this.cy = 50;
+        this.render = function () {
+            return `<circle cx="${this.cx}" cy="${this.cy}" r="${this.radius}" fill="${this.color}"></circle>`;
+        }
+    }
+}
+
+class Square extends Shapes {
+    constructor(color) {
+        super(color);
+        this.width = 100;
+        this.height = 100;
+        this.x = 0;
+        this.y = 0;
+        this.render = function () {
+            return `<rect x="${this.x}" y="${this.y}" width="${this.width}" height="${this.height}" fill="${this.color}"></rect>`;
+        }
+    }
+}
+
+class Triangle extends Shapes {
+    constructor(color) {
+        super(color);
+        this.points = "50,0 0,100 100,100";
+        this.render = function () {
+            return `<polygon points="${this.points}" fill="${this.color}"></polygon>`;
+        }
+    }
+}
+
+module.exports = { Circle, Square, Triangle };
